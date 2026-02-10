27f1a37f804b0599593a6e0d06f03f7e9bcab4ad
irem1cakmak <124272113+irem1cakmak@users.noreply.github.com>
2026-02-10 21:15:54 +0300

    Constructor overloading in Employee class

--- src/Main.java
+++ src/Main.java
@@ -17,14 +17,33 @@ public class Main {
         System.out.print("Enter the task: ");
         String task=scanner.nextLine();
         Employee e=new Employee(name,surname,salary,country,task);
-
-
         System.out.println("---");
         System.out.println("Name: " + e.name);
         System.out.println("Surname: " + e.surname);
         System.out.println("Salary: " + e.salary);
         System.out.println("Country: " + e.country);
         System.out.println("Task: " + e.task);
+        System.out.println("---");
+
+        System.out.print("Enter the name: ");
+        String name1=scanner.nextLine();
+        System.out.print("Enter the salary: ");
+        int salary1=scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Enter the task: ");
+        Employee e1=new Employee(name1,salary1);
+        System.out.println("---");
+        System.out.println("Name: " + e1.name);
+        System.out.println("Salary: " + e1.salary);
+        System.out.println("---");
+        System.out.print("Enter the name: ");
+        String name2=scanner.nextLine();
+        Employee e2=new Employee(name2);
+
+        System.out.println("---");
+        System.out.println("Name: " + e2.name);
+        System.out.println("Salary: " + e2.salary);
+
 
 
     }
